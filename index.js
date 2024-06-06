d5f366892e09d480b333250ae1681c7a63340842
Nathan Han <nh0713@uw.edu>
2024-06-06 12:27:16 -0700

    Add PUT and DELETE

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const port = 3000;
 // Not allowed to pass JSON in body by default; must setup middleware.
 app.use(express.json());
 
-// MongoDB Cluster Username: nathan0713
-// MongoDB Cluster PW: IAVHNsyoMyvaH7Nb
-
 app.get('/', (req, res) => {
   res.send("Hello from Node API Server");
 });
@@ -24,7 +21,7 @@ app.get('/api/products', async (req, res) => {
   }
 });
 
-app.get('/api/products/:id', async (req, res) => {
+app.get('/api/product/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const product = await Product.findById(id);
@@ -38,6 +35,32 @@ app.post('/api/products', async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
+});
+
+app.put('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.status(404).json({message: "Product not found"});
+    }
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
+});
+
+app.delete('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({message: "Product not found"});
+    }
+    res.status(200).json({message: "Product deleted successfully"});
   } catch (err) {
     res.status(500).json({message: err.message});
   }
