298329d49c356bd0eb5f8cfc648740c2815aa7c6
Nathan Han <nh0713@uw.edu>
2024-06-05 22:01:56 -0700

    Setup and connect to MongoDB

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-});
+// MongoDB Cluster Username: nathan0713
+// MongoDB Cluster PW: IAVHNsyoMyvaH7Nb
 
 app.get('/', (req, res) => {
-  res.send("Hello from Node API");
+  res.send("Hello from Node API Server");
 });
 
-
+mongoose.connect("mongodb+srv://nathan0713:IAVHNsyoMyvaH7Nb@cluster0.v5gvadm.mongodb.net/Node-Express-API")
+.then(() => {
+  console.log("Connected to database!");
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+  });
+})
+.catch(() => {
+  console.log("Connection failed");
+});
