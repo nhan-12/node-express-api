751d19e4f79835548d7a18ef8e488b0b8e1b4e7a
Nathan Han <nh0713@uw.edu>
2024-06-05 23:29:26 -0700

    Create model and some get and post enpoints

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+
 const app = express();
 const port = 3000;
+
+// Not allowed to pass JSON in body by default; must setup middleware.
+app.use(express.json());
 
 // MongoDB Cluster Username: nathan0713
 // MongoDB Cluster PW: IAVHNsyoMyvaH7Nb
 
 app.get('/', (req, res) => {
   res.send("Hello from Node API Server");
+});
+
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
+});
+
+app.get('/api/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
+});
+
+app.post('/api/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
 });
 
 mongoose.connect("mongodb+srv://nathan0713:IAVHNsyoMyvaH7Nb@cluster0.v5gvadm.mongodb.net/Node-Express-API")
